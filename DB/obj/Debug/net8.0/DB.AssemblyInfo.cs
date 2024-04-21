25b368bd1cdaf1b7ef762d29e49550a93022596b
Christian Iglesias <59263708+ChristGG1997@users.noreply.github.com>
2024-04-20 22:39:43 -0500

    CHORE: Agrega y se eliminan archivos

--- DB/obj/Debug/net8.0/DB.AssemblyInfo.cs
+++ DB/obj/Debug/net8.0/DB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d4225f7489b7e3614cdd7826548c10cb3cd137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32be25d2c3710dcf6cfdb1a86936bf82c39e56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0/AppAgendaContactosApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AppAgendaContactosApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAgendaContactosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32be25d2c3710dcf6cfdb1a86936bf82c39e56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAgendaContactosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAgendaContactosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
